154431e29815ac9341c0771bb6760d2678b4f776
davids <dheo0@naver.com>
2023-05-10 14:45:46 +0900

    useState test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from './hello';
+import Wrapper from './Wrappers';
+import Counter from './Couter';
+import InputSample from './InputSample';
+import './App.css'
 
 function App() {
+  const name ='Oslo'
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Hello name='react' color='red' isSpecial={false}/>
+      <Hello color='pink' />
+      <Counter />
+      <InputSample />
+    </Wrapper>
   );
 }
 
